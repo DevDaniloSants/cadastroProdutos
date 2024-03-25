f6e67c2322f51479019ab24a6c5a69c08cb9fae3
Danilo Santos <dscofield@outlook.com.br>
2024-03-24 21:35:18 -0300

    feat: create the add product button

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,11 +1,22 @@
 import Table from "../components/Table";
 
+import { useNavigate } from "react-router-dom";
+
 const Products = () => {
+    const navigate = useNavigate();
+
+    const handleClick = () => {
+        navigate("/addProduct");
+    };
+
     return (
         <div>
             <div className="description">
                 <h1>Produtos</h1>
                 <p>Área destinada a visualização de todos os produtos</p>
+                <button className="addProduct" onClick={handleClick}>
+                    Adicionar Produto
+                </button>
             </div>
             <Table />
         </div>
